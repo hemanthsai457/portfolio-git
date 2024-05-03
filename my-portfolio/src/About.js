61b56076533c1579abc08450d4bf4b3497626c33
Hemanth Sai Gokarakonda <hemanthsai457@gmail.com>
2024-05-03 02:04:46 -0700

    first commit

---
+++ my-portfolio/src/About.js
@@ -0,0 +1,14 @@
+
+const About = () => {
+    return ( 
+      <div className="About">
+        <h1>About Me!</h1>
+        <div className="about-inner">
+          
+        </div>
+      </div>
+     );
+}
+ 
+
+export default About;
