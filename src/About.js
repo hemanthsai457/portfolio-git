5d402e0508c27f52ce1525f3347fe38caf6d9c4d
Hemanth Sai Gokarakonda <hemanthsai457@gmail.com>
2024-05-03 19:14:11 -0700

    shooting_star_implementation

---
+++ src/About.js
@@ -0,0 +1,19 @@
+
+const About = () => {
+    return ( 
+      <div className="About-outer">
+        <h1>About Me!</h1>
+        <div className="lights-cntr">
+          <div className="l1"></div>
+          <div className="myimg2"></div>
+          <div className="l2"></div>
+        </div>
+        <div className="about-data">
+          <p>Experienced professional working as a Student Assistant at Forty-Niner Shops and a Master's degree candidate in Computer Science at CSULB, I am passionate about learning and applying new technologies to solve real-world problems. I have over two years of experience in software development, working with Python, Full Stack, AWS, Salesforce, SQL, ETL, Java, and AI across various domains and industries. Experienced professional working as a Student Assistant at Forty-Niner Shops and a Master's degree candidate in Computer Science at CSULB, I am passionate about learning and applying new technologies to solve real-world problems. I have over two years of experience in software development, working with Python, Full Stack, AWS, Salesforce, SQL, ETL, Java, and AI across various domains and industries. </p>
+        </div>
+      </div>
+     );
+}
+ 
+
+export default About;
