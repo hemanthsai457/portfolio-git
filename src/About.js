e3c600d1d22ab9caaed3cdaf3c46261d0f44c765
Hemanth Sai Gokarakonda <hemanthsai457@gmail.com>
2024-05-08 14:57:00 -0700

    lights implementation - phase 1

--- src/About.js
+++ src/About.js
@@ -6,7 +6,7 @@ const About = () => {
         <div className="lights-cntr">
           <div className="l1"></div>
           <div className="myimg2"></div>
-          <div className="l2"></div>
+          <div className="l1"></div>
         </div>
         <div className="about-data">
           <p>Experienced professional working as a Student Assistant at Forty-Niner Shops and a Master's degree candidate in Computer Science at CSULB, I am passionate about learning and applying new technologies to solve real-world problems. I have over two years of experience in software development, working with Python, Full Stack, AWS, Salesforce, SQL, ETL, Java, and AI across various domains and industries. Experienced professional working as a Student Assistant at Forty-Niner Shops and a Master's degree candidate in Computer Science at CSULB, I am passionate about learning and applying new technologies to solve real-world problems. I have over two years of experience in software development, working with Python, Full Stack, AWS, Salesforce, SQL, ETL, Java, and AI across various domains and industries. </p>
@@ -14,6 +14,5 @@ const About = () => {
       </div>
      );
 }
- 
 
 export default About;
