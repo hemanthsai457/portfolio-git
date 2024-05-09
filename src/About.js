c0ba239df4b61fa15f5b7597b750af9455f2dc32
Hemanth Sai Gokarakonda <hemanthsai457@gmail.com>
2024-05-08 18:57:30 -0700

    lights-phase2

--- src/About.js
+++ src/About.js
@@ -1,15 +1,33 @@
+import Switch from '@mui/material/Switch';
+import * as React from 'react';
+
+const label = { inputProps: { 'aria-label': 'Switch' } };
 
 const About = () => {
+
+  const [isLightOn, setLightOn] = React.useState(false);
+  const toggleLight =()=>{
+    setLightOn(!isLightOn);
+  };
     return ( 
       <div className="About-outer">
         <h1>About Me!</h1>
         <div className="lights-cntr">
-          <div className="l1"></div>
+          <div className="l1">
+            <div className={`yellow-light ${isLightOn ? 'visible' : ''}`}></div>
+          </div>
           <div className="myimg2"></div>
-          <div className="l1"></div>
+          <div className="lightswitch">
+            <Switch {...label} checked={isLightOn} onChange={toggleLight}/>
+          </div>
+          <div className="l1">
+          <div className={`yellow-light ${isLightOn ? 'visible' : ''}`}></div>
+          </div>
         </div>
-        <div className="about-data">
+        <div className='about-data'>
+        <div className={`text-appear ${isLightOn ? 'visible':''}`}>
           <p>Experienced professional working as a Student Assistant at Forty-Niner Shops and a Master's degree candidate in Computer Science at CSULB, I am passionate about learning and applying new technologies to solve real-world problems. I have over two years of experience in software development, working with Python, Full Stack, AWS, Salesforce, SQL, ETL, Java, and AI across various domains and industries. Experienced professional working as a Student Assistant at Forty-Niner Shops and a Master's degree candidate in Computer Science at CSULB, I am passionate about learning and applying new technologies to solve real-world problems. I have over two years of experience in software development, working with Python, Full Stack, AWS, Salesforce, SQL, ETL, Java, and AI across various domains and industries. </p>
+        </div>
         </div>
       </div>
      );
